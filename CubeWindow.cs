71e1956fa08785526586935821a47571b64496b1
Oleg Vysotsky <olegbom@gmail.com>
2024-01-26 16:47:45 +0300

    refactoring

--- CubeWindow.cs
+++ CubeWindow.cs
@@ -4,41 +4,6 @@ using Terminal.Gui;
 
 namespace CubeConsole;
 
-public class CubeStack
-{
-
-    private int _size = 0;
-    private List<byte[,,]> _stack = new ();
-
-    public void Push(byte[,,] cube)
-    {
-        _size++;
-        if (_size > _stack.Count)
-        {
-            byte[,,] b = (byte[,,]) cube.Clone();
-            _stack.Add(b);
-        }
-        else
-        {
-            Array.Copy(cube, _stack[_size-1], cube.Length);
-        }
-    }
-
-    public void Pop(byte[,,] cube)
-    {
-        if (_size > 0)
-        {
-            Array.Copy(_stack[_size - 1], cube, cube.Length);
-            _size--;
-        }
-        else
-        {
-            throw new Exception("Pop without push!");
-        }
-    }
-}
-
-
 public class CubeWindow:Window
 {
 
